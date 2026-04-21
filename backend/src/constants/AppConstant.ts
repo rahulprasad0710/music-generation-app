6646ce21c4e9b1cebda8c96d94ab766116411eee
rahulprasad0710 <raulshah2021@hotmail.com>
2026-04-21 12:09:14 +0545

    fix: change redis from  local to docker

--- backend/src/constants/AppConstant.ts
+++ backend/src/constants/AppConstant.ts
@@ -16,8 +16,8 @@ export const APP_CONSTANT = {
     DATABASE: process.env.DATABASE,
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
-    // REDIS_HOST: process.env.REDIS_HOST || "localhost",
-    REDIS_HOST: "localhost",
+    REDIS_HOST: process.env.REDIS_HOST || "localhost",
+    // REDIS_HOST: "localhost",
 
     REDIS_PORT: process.env.REDIS_PORT || 6379,
     FRONTEND_BASE_URL: process.env.FRONTEND_BASE_URL,
